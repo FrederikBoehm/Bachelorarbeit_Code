336fb48acf40ae3cc131e5a8b02c38b9ed515ce8
Frederik Böhm <boehmfr68270@th-nuernberg.de>
2019-09-26 16:53:55 +0200

    fixed multiline bug in prepare_data_for_bert.py

--- prepare_data_for_bert.py
+++ prepare_data_for_bert.py
@@ -17,7 +17,7 @@ def prepareDataForBert():
     # Filter for reports after 31.12.2003 and only 10-K's and 10-Q's
     df_file_summaries = df_file_summaries[df_file_summaries['FILING_DATE'].astype(str).str.contains(r'20(?:04|05|06|07|08|09|10|11|12|13|14|15|16|17|18)\S{4}')]
     df_file_summaries = df_file_summaries[(df_file_summaries['FORM_TYPE'] == '10-K') | (df_file_summaries['FORM_TYPE'] == '10-Q')]
-
+    df_cik_ticker_mapping = pd.read_csv('./data/cik_ticker_modified.csv', delimiter='|')
 
     checkpoint = {}
     checkpoint_file = open('./data-preparation.checkpoint.json', 'r') 
@@ -30,6 +30,12 @@ def prepareDataForBert():
     # count = 0
     count = checkpoint["count"] if checkpoint["count"] else 0
     init_index = checkpoint["index"] if checkpoint["index"] else 0
+
+    if init_index == 0:
+        multiline_report_index = open('./data/multiline_report_index.csv', 'w')
+        multiline_report_index.write('CIK\tTicker\tCompany\tFiling_Date\tFile_Path\n')
+        multiline_report_index.close()
+        
     for index, cik in enumerate(df_file_summaries['CIK'][init_index:], start=init_index):
         # if historic_composition_at_date.contains(cik):
         if cik in historic_composition_at_date:
@@ -38,31 +44,37 @@ def prepareDataForBert():
             report_file_path = report_file_path.replace('D:', './data').replace('\\', '/')
             
             f = open(report_file_path, 'r')
-            if f.mode == 'r':
-                report = f.read()
+            report = f.read()
 
-                print('Starting preprocessing for ' + report_file_path)
-                report = _removeHeader(report)
-                report = _removeExhibits(report)
-                report = _makeSingleLine(report)
+            print('Starting preprocessing for ' + report_file_path)
+            report = _removeHeader(report)
+            report = _removeExhibits(report)
+            report = _makeSingleLine(report)
 
-                sentences = _splitReportToSentences(report)                
+            sentences = _splitReportToSentences(report)                
 
-                seperator = '\n'
-                multiline_report = seperator.join(sentences)
+            seperator = '\n'
+            multiline_report = seperator.join(sentences)
 
-                if not os.path.exists('./data/multiline_reports'):
-                    os.makedirs('./data/multiline_reports')
+            if not os.path.exists('./data/multiline_reports'):
+                os.makedirs('./data/multiline_reports')
 
-                output_file_path = './data/multiline_reports/multiline_report' + str(count)
-                output_file = open(output_file_path, 'w+')
-                output_file.write(multiline_report)
-                output_file.close()
-                count = count + 1
-                print('Wrote processed report to ' + output_file_path)
+            output_file_path = './data/multiline_reports/multiline_report' + str(count)
+            output_file = open(output_file_path, 'w+')
+            output_file.write(multiline_report)
+            output_file.close()
+            count = count + 1
+            print('Wrote processed report to ' + output_file_path)
 
-            else:
-                print('Unable to read file ' + report_file_path)
+            print('Building Index file...')
+            filing_date = df_file_summaries['FILING_DATE'].iloc[index]
+            ticker = df_cik_ticker_mapping.loc[df_cik_ticker_mapping['CIK'] == cik, 'Ticker'].iloc[0]
+            company = df_cik_ticker_mapping.loc[df_cik_ticker_mapping['CIK'] == cik, 'Name'].iloc[0]
+            print(f'Adding entry with CIK {cik}, Ticker {ticker}, Company {company}, Filing Date {filing_date}, Output File Path {output_file_path} .')
+            multiline_report_index = open('./data/multiline_report_index.csv', 'a')
+            multiline_report_index.write(f'{cik}\t{ticker}\t{company}\t{filing_date}\t{output_file_path}\n')
+            multiline_report_index.close()
+
 
         if (df_file_summaries['FILING_DATE'].iloc[index] != historic_composition_date) & (df_historic_composition['Date'].eq(df_file_summaries['FILING_DATE'].iloc[index]).any()):
             historic_composition_date = df_file_summaries['FILING_DATE'].iloc[index]
@@ -108,8 +120,7 @@ def _splitReportToSentences(report):
     sentences = []
     if len(report) < nlp.max_length:
         doc = nlp(report)
-        for sentence in doc.sents:
-            sentences.extend(sentence.text)
+        sentences = [sent.string.strip() for sent in doc.sents]
     else:
         print(f'Document too long, processing in two parts...')
         # If the report is longer than what spacy can process we first split the report in half and process the first part...
